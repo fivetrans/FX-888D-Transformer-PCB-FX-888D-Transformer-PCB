0c1c392b8dec3b8bb2a9cff451cc41be6644c07a
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-10-18 16:30:46 +0000

    Delete launch.json

---
+++ src/cpp/.devcontainer/reinstall-cmake.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=20903
