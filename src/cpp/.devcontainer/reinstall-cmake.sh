afc9bb36defdb7147cc9eac47979864c97eb047f
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-10-21 17:42:39 +0000

    sparc64: Fix race in TLB batch processing.

--- src/cpp/.devcontainer/reinstall-cmake.sh
+++ src/cpp/.devcontainer/reinstall-cmake.sh
@@ -43,4 +43,11 @@ case "${architecture}" in
 esac
 
 CMAKE_BINARY_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}.sh"
-CMAKE_CHECKSUM_NAME="cmake-${CMAKE_VERSION}-SHA-256.t
+CMAKE_CHECKSUM_NAME="cmake-${CMAKE_VERSION}-SHA-256.txt"
+TMP_DIR=$(mktemp -d -t cmake-XXXXXXXXXX)
+
+echo "${TMP_DIR}"
+cd "${TMP_DIR}"
+
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_BINARY_NAME}" -O
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_CHECKSUM_NAME}"
