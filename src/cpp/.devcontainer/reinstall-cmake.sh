f9a988da8f72ed0504a447951c778c47295603f2
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-10-21 10:40:26 +0000

    CHANGELOG updated.

--- src/cpp/.devcontainer/reinstall-cmake.sh
+++ src/cpp/.devcontainer/reinstall-cmake.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=20903
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+#
+set -e
+
+CMAKE_VERSION=${1:-"none"}
+
+if [ "${CMAKE_VERSION}" = "none" ]; then
+    echo "No CMake version specified, skipping CMake reinstallation"
+    exit 0
+fi
+
+# Cleanup temporary d
