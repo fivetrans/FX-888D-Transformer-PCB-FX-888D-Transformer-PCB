42df01e03716efc5340da931efdc87fcded498f8
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-12-08 15:36:17 +0000

    Create CODEOWNERS

--- src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
+++ src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
@@ -2,4 +2,17 @@
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#---------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+#
+set -e
+
+CMAKE_VERSION=${1:-"none"}
+
+if [ "${CMAKE_VERSION}" = "none" ]; then
+    echo "No CMake version specified, skipping CMake reinstallation"
+    exit 0
+fi
+
+# Cleanup temporary directory and associated files when exiting the script.
+cleanup() {
+    EXIT_CODE=$
