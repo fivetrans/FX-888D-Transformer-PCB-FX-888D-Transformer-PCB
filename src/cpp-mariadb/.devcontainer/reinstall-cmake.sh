c832bc1d5fc040a5fc0d737f1142a38744c67b60
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-12-12 16:38:57 +0000

    * code review

--- src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
+++ src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
@@ -31,4 +31,16 @@ apt-get -y purge --auto-remove cmake
 mkdir -p /opt/cmake
 
 architecture=$(dpkg --print-architecture)
-case "${architectu
+case "${architecture}" in
+    arm64)
+        ARCH=aarch64 ;;
+    amd64)
+        ARCH=x86_64 ;;
+    *)
+        echo "Unsupported architecture ${architecture}."
+        exit 1
+        ;;
+esac
+
+CMAKE_BINARY_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}.sh"
+CMAKE_CHECKSUM_NAME="cmake-${CMAKE_VERSION}-SHA-256.txt"
