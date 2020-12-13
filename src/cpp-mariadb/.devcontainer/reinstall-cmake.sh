25830344c63b91b6addcb018941d3a693a5420c2
fivetrans <124847521+fivetrans@users.noreply.github.com>
2020-12-13 12:00:13 +0000

    0.5.25

--- src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
+++ src/cpp-mariadb/.devcontainer/reinstall-cmake.sh
@@ -44,3 +44,12 @@ esac
 
 CMAKE_BINARY_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}.sh"
 CMAKE_CHECKSUM_NAME="cmake-${CMAKE_VERSION}-SHA-256.txt"
+TMP_DIR=$(mktemp -d -t cmake-XXXXXXXXXX)
+
+echo "${TMP_DIR}"
+cd "${TMP_DIR}"
+
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_BINARY_NAME}" -O
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_CHECKSUM_NAME}" -O
+
+sha256sum -c --ignore
