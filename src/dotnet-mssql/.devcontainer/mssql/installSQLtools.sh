50a12d1e4a9b8e1c871c378462a81db63c11f14a
fivetrans <124847521+fivetrans@users.noreply.github.com>
2021-06-12 08:48:55 +0000

    Add latest Ansible versions (2.5.5, 2.4.5.0)

--- src/dotnet-mssql/.devcontainer/mssql/installSQLtools.sh
+++ src/dotnet-mssql/.devcontainer/mssql/installSQLtools.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Installing mssql-tools"
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | (OUT=$(apt-key add - 2>&1
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | (OUT=$(apt-key add - 2>&1) || echo $OUT)
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-${DISTRO}-${CODENAME}-prod ${CODENAME} main" > /etc/apt/sources.list.d/microsoft.list
+apt-get update
+ACCEPT_EULA=Y apt-get -y install unixodbc-dev msodbcsql17 libun
