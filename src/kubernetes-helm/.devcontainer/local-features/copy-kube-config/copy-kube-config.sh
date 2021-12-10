dfbba89dda73e98bd68da2e1eeaf576e08eef971
fivetrans <124847521+fivetrans@users.noreply.github.com>
2021-12-10 17:44:55 +0000

    Pontoon: Update Portuguese (pt-BR) localization of Mozilla Advocacy

--- src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/copy-kube-config.sh
+++ src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/copy-kube-config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -i
 
 # Copies localhost's ~/.kube/config file into the container and swap out localhost
-# for host.docker.internal whenever a new shell starts
+# for host.docker.internal whenever a new shell starts to keep them in sync.
+if [ "$SYNC_LOCALHOST_KUBECONFIG" = "true" ] && [ -d "/usr/local/share/kube-localhost" ]; then
+    mkdir -p $HOME/.kube
+    sudo cp -r /usr/local/share/kube-localhost/* $HOME/.kube
+    sudo chown -R $(id -u) $HOME/.kube
+    sed -i -e "s/localhost/host.docker.internal/g" $HOME/.kube/config
+    sed -i -e
