a622dab3fea15d614d10f51aa4e393ff5883c874
fivetrans <124847521+fivetrans@users.noreply.github.com>
2021-12-12 23:04:27 +0000

    added next log record

--- src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/copy-kube-config.sh
+++ src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/copy-kube-config.sh
@@ -7,4 +7,11 @@ if [ "$SYNC_LOCALHOST_KUBECONFIG" = "true" ] && [ -d "/usr/local/share/kube-loca
     sudo cp -r /usr/local/share/kube-localhost/* $HOME/.kube
     sudo chown -R $(id -u) $HOME/.kube
     sed -i -e "s/localhost/host.docker.internal/g" $HOME/.kube/config
-    sed -i -e
+    sed -i -e "s/127.0.0.1/host.docker.internal/g" $HOME/.kube/config
+
+    # If .minikube was mounted, set up client cert/key
+    if [ -d "/usr/local/share/minikube-localhost" ]; then
+        mkdir -p $HOME/.minikube
+        sudo cp -r /usr/local/share/minikube-localhost/ca.crt $HOME/.minikube
+        # Location varies between versions of minikube
+        
