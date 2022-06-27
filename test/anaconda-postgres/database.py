2a044bc09f0098ef522ddde7cee8673f32700cbe
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-06-27 15:41:23 +0000

    Adds comments

--- test/anaconda-postgres/database.py
+++ test/anaconda-postgres/database.py
@@ -3,4 +3,17 @@
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#---------
+#-------------------------------------------------------------------------------------------------------------
+
+#%%
+import os
+import sys
+import traceback
+
+import psycopg2
+
+# Connect to the database
+pg_user = os.getenv('POSTGRES_USER')
+assert pg_user is not None, "POSTGRES_USER environment variable not set"
+
+pg_pas
