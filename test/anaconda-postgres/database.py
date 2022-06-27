effa563596cf08d36436030c1b8e90c2f5a4f439
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-06-27 07:09:00 +0000

    update time

--- test/anaconda-postgres/database.py
+++ test/anaconda-postgres/database.py
@@ -1,1 +1,6 @@
-# pylint: disable=line-too-long, missing-module-docstring, broad-except, con
+# pylint: disable=line-too-long, missing-module-docstring, broad-except, consider-using-f-string
+
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#---------
