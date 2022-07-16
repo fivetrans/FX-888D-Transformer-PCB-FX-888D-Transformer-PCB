70319702a085b4ae2b505f5930d8ea1cc59eb338
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-07-16 16:08:43 +0000

    espn

--- test/anaconda-postgres/plot.py
+++ test/anaconda-postgres/plot.py
@@ -2,4 +2,15 @@
 
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt
