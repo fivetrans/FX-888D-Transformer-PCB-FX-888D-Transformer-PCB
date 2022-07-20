bba28a01824a88e8ffdddc9af5f010d87ecd0d2b
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-07-20 13:56:57 +0000

    Bump jackson-databind from 2.9.10.1 to 2.9.10.3

--- test/anaconda-postgres/plot.py
+++ test/anaconda-postgres/plot.py
@@ -13,4 +13,14 @@ import numpy as np
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
-fig, ax = plt
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("plot.png")
+plt.show()
+
+print('Open test-project/plot.png to see the result!')
---
+++ test/anaconda-postgres/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+# Run common tests
+checkCommo
