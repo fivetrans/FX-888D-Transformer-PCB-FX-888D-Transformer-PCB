b3257d81d4ca3c0d8d1b5fff1d08d6bc3b59e96c
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-07-22 15:26:30 +0000

    Fix flaky test.

--- test/cpp/test.sh
+++ test/cpp/test.sh
@@ -11,4 +11,7 @@ checkCommon
 
 # Run template specific tests
 checkExtension "ms-vscode.cpptools"
-checkOSPackages "command-line-
+checkOSPackages "command-line-tools" build-essential cmake cppcheck valgrind clang lldb llvm gdb
+checkOSPackages "tools-for-vcpkg" tar curl zip unzip pkg-config bash-completion ninja-build
+VCPKG_UNSUPPORTED_ARM64_VERSION_CODENAMES="stretch bionic"
+if [ "$(dpkg --print-architecture)" = "amd64" ] || [[ ! "${VCPKG_UNSUPPORTED_ARM64_VERSION_COD
