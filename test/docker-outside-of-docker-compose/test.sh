f3a3de0906d8a43b2b721948ccf4599d6d7ed1f7
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-07-29 06:49:16 +0000

    Version 1.1r39

--- test/docker-outside-of-docker-compose/test.sh
+++ test/docker-outside-of-docker-compose/test.sh
@@ -3,4 +3,15 @@ cd $(dirname "$0")
 
 source test-utils.sh vscode
 
-# Remote - Containers does not auto-sync UID/GID for Doc
+# Remote - Containers does not auto-sync UID/GID for Docker Compose,
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExtension "ms-azuretools.vscode-docker"
+check "docker" docker ps -a
+check "docker-compose" docker-compose --version
+check "do
