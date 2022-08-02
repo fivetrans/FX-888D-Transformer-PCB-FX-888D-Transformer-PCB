d361d69a61e3c0e09a2f3640c3b53a886c71ea45
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-08-02 21:56:58 +0000

    New Icon

--- test/dotnet/Program.cs
+++ test/dotnet/Program.cs
@@ -1,3 +1,14 @@
 /*-------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?l
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace aspnetapp
+{
+    public class Program
+    {
+        public s
