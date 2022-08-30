e57fc9232b4fb6fb8c584e2a75883c6e331ad269
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-08-30 15:20:40 +0000

    Bump version #

--- test/dotnet/test.sh
+++ test/dotnet/test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 cd $(dirname "$0")
 
-source test-
+source test-utils.sh vscode
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExtension "ms-dotnettools.csharp"
+check "dotnet" dotnet --info
+check "nuget" dotnet restore
+check "msbuild" dotnet msbuild
+sudo rm -rf obj bin
+
+# Report result
+reportResults
