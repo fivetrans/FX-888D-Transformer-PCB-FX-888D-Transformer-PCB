9cbf3c9671c8a94c96b838d123e86b29a6444031
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-09-03 21:37:47 +0000

    init repository

---
+++ test/dotnet-fsharp/test.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExtension "ms-dotnettools.csharp"
+checkExtension "ionide.ionide-fsharp"
+check "dotnet" dotnet --info
+check "nuget" dotnet restore
+check "msbuild" dotnet msbuild
+sudo rm -rf obj bin
+
+# Report result
+reportResults
