0955c096e509f1bf52a4a7862812e3b71781cc8f
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-09-21 16:39:11 +0000

    more typos

--- test/java/src/main/java/com/mycompany/app/App.java
+++ test/java/src/main/java/com/mycompany/app/App.java
@@ -3,4 +3,15 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-package com.mycompan
+package com.mycompany.app;
+
+import static javax.xml.XMLConstants.XML_NS_PREFIX;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Remote World!" );
+        System.out.println("The XML namespace prefix is: " + XML_NS_PREFIX);
+    }
+}
