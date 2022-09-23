2beddf46bd10fe617f7f2d66eab71bf6a17744c6
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-09-23 13:45:36 +0000

    Feature updates

--- test/java/test.sh
+++ test/java/test.sh
@@ -8,4 +8,12 @@ checkCommon
 
 # Prep
 echo -e "\nGetting Maven wrapper..."
-curl -sSL https://github.com/takari/maven-wrapper
+curl -sSL https://github.com/takari/maven-wrapper/archive/maven-wrapper-0.5.5.tar.gz| tar -xzf - 
+mv maven-wrapper-maven-wrapper-0.5.5/mvnw mvnw
+mv maven-wrapper-maven-wrapper-0.5.5/.mvn .mvn
+rm -rf mv maven-wrapper-maven-wrapper-0.5.5
+
+# template specific tests
+checkExtension "vscjava.vscode-java-pack"
+check "java" java -version
+check "build-and-test-jar" ./mvnw -q pack
