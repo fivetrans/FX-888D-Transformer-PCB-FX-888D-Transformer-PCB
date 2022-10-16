adef9648dd820a19adb3c938f2e3ff401a2d4051
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-10-16 18:53:43 +0000

    Update README.md [skip ci]

--- test/java-8/src/test/java/com/mycompany/app/AppTest.java
+++ test/java-8/src/test/java/com/mycompany/app/AppTest.java
@@ -12,4 +12,17 @@ import static org.junit.Assert.*;
 public class AppTest
 {
     public AppTest() {
- 
+    }
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testMore()
+    {
+        assertTrue( true );
+    }
+}
