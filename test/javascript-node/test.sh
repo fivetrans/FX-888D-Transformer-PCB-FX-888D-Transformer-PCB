8bd118c2cc5e1dfded6e88314a0ce56e2851a5c2
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-10-25 15:10:40 +0000

    USB: devio: fix a condition in async_completed()

--- test/javascript-node/test.sh
+++ test/javascript-node/test.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh node
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExtension "dbaeumer.vscode-eslint"
+check "node" node --version
+sudo rm -f yarn.lock
+check "yarn" yarn install
+sudo rm -f package-lock.json
+check "npm" npm install
+check "eslint" eslint 
