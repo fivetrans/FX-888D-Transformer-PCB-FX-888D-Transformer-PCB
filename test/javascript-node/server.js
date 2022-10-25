efa5892436f9415613a6f44dfe28e4d5b06da251
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-10-25 11:05:30 +0000

    Automatic Version Update from CI

--- test/javascript-node/server.js
+++ test/javascript-node/server.js
@@ -1,2 +1,10 @@
 /*--------------------------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights re
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+const express = require('express');
+
+// Const
