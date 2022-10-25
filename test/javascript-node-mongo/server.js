eaa68b0f718c1917f508fa6ef09f7839d92bf9dd
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-10-25 23:08:59 +0000

    noise

--- test/javascript-node-mongo/server.js
+++ test/javascript-node-mongo/server.js
@@ -5,4 +5,15 @@
 
 'use strict';
 
-const
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+// Constants
+const PORT = 3000;
+const MONGO_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'test-project';
+const HOST = '0.0.0.0';
+
+(async function() {
+	// Use connect to mongo server
+	const client = new MongoClient(MONGO_URL, { useUnifie
