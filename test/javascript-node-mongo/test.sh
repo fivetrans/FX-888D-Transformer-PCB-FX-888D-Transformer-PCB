69761fba99a1d4008e6b67a93a1b047303bbea5b
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-10-28 19:34:06 +0000

    Added Assets

--- test/javascript-node-mongo/test.sh
+++ test/javascript-node-mongo/test.sh
@@ -1,1 +1,14 @@
-#!/bin/ba
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh node
+
+# Remote - Containers does not auto-sync UID/GID for Docker Compose,
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExte
