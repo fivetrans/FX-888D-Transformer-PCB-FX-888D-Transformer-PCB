3b066611d8116b8f262447cab7a703bf29297d71
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-11-10 10:30:39 +0000

    Remove CVS-Ids.

--- test/javascript-node-mongo/test.sh
+++ test/javascript-node-mongo/test.sh
@@ -11,4 +11,14 @@ fixTestProjectFolderPrivs
 checkCommon
 
 # template specific tests
-checkExte
+checkExtension "dbaeumer.vscode-eslint"
+check "node" node --version
+sudo rm -f yarn.lock
+check "yarn" yarn install
+sudo rm -f package-lock.json
+check "npm" npm install
+check "eslint" eslint server.js
+check "test-project" npm run test
+npm config delete prefix
+check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 8"
+check "nv
