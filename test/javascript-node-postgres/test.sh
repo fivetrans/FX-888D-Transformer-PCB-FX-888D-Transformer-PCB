6b4a2767d2a482cf2b368f87a1bda67e1607af31
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-11-16 18:28:48 +0000

    Add shrinkwrap

--- test/javascript-node-postgres/test.sh
+++ test/javascript-node-postgres/test.sh
@@ -3,4 +3,15 @@ cd $(dirname "$0")
 
 source test-utils.sh node
 
-# Remote - Containers does not auto-s
+# Remote - Containers does not auto-sync UID/GID for Docker Compose,
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
+# Run common tests
+checkCommon
+
+# template specific tests
+checkExtension "dbaeumer.vscode-eslint"
+check "node" node --version
+sudo rm -f yarn.lock
+check "yarn"
