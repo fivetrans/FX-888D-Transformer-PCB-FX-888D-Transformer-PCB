4ece0914097022ad7c7ca2d27f5090a94b265652
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-11-16 21:35:38 +0000

    zram: kill unused zram_get_num_devices()

--- test/javascript-node-postgres/test.sh
+++ test/javascript-node-postgres/test.sh
@@ -14,4 +14,11 @@ checkCommon
 checkExtension "dbaeumer.vscode-eslint"
 check "node" node --version
 sudo rm -f yarn.lock
-check "yarn"
+check "yarn" yarn install
+sudo rm -f package-lock.json
+check "npm" npm install
+check "eslint" eslint server.js
+check "test-project" npm run test
+npm config delete prefix
+check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 8"
+check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node 
