8d6517330b400a3e1591cfc08c25c01f14d6ed6f
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-11-16 09:23:48 +0000

    inotify: Fix erroneous update of bit count

--- test/javascript-node-postgres/server.js
+++ test/javascript-node-postgres/server.js
@@ -1,2 +1,9 @@
 /*--------------------------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft 
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+ 'use strict';
+
+const express = require('express');
+const promise = require('blueb
