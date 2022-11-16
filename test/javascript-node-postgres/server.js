2000e9c59a625e304074b8569c08e43ff03613f6
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-11-16 09:39:22 +0000

    Remove needless code

--- test/javascript-node-postgres/server.js
+++ test/javascript-node-postgres/server.js
@@ -6,4 +6,17 @@
  'use strict';
 
 const express = require('express');
-const promise = require('blueb
+const promise = require('bluebird');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+const cn = {
+	host: 'localhost', // host of db container
+	port: 5432, // 5432 is the default;
+	database: 'postgres', // database name
+	user: 'postgres', // database user name
+	password: 'postgres' // database password
+};
+const initOptions = {
+	promiseLib:
