f6f2f2885c7303e043c77f938319d426b67417a3
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-12-13 05:38:46 +0000

    Add missing license file

--- test/php-mariadb/test.sh
+++ test/php-mariadb/test.sh
@@ -4,4 +4,16 @@ cd $(dirname "$0")
 source test-utils.sh vscode
 
 # Remote - Containers does not auto-sync UID/GID for Docker Compose,
-# so make sure test project prvs 
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
+# Run common tests
+checkCommon
+
+# Actual tests
+checkExtension "felixfbecker.php-debug"
+checkExtension "felixfbecker.php-intellisense"
+checkExtension "mrmlnc.vscode-apache"
+check "php" php --version
+check "apache2ctl" which apache2ctl
+sleep 15 # Sleep to be 
