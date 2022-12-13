0858940d8eda9b36d520217c4ba316214ecac432
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-12-13 13:47:01 +0000

    Remove unnecessary dependencies

---
+++ test/rust-postgres/src/main.rs
@@ -0,0 +1,3 @@
+/*--------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid
