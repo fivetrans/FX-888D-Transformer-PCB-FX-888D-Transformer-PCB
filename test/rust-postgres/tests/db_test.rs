2438c762cd2b446fcdd484e52a219b8e8e4d404c
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-12-13 18:16:58 +0000

    Update common Prometheus files

--- test/rust-postgres/tests/db_test.rs
+++ test/rust-postgres/tests/db_test.rs
@@ -1,4 +1,15 @@
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------
+ *-------------------------------------------------------------------------------------------------------------*/
+
+use std::{env, net::TcpStream};
+extern crate postgres;
+use postgres::{Client, NoTls};
+
+fn getenv(name: &str) -> String {
+    let val = match env::var(name) {
+        Ok(val) => val,
+        Err(err) => panic!("Error {} {:?}", name, err),
+    };
+   
