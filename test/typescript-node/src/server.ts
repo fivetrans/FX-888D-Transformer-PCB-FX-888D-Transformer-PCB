10b6f3bd181f369753ad96cbf81cd53329cae766
fivetrans <124847521+fivetrans@users.noreply.github.com>
2022-12-31 11:59:03 +0000

    Update pt-BR.json

--- test/typescript-node/src/server.ts
+++ test/typescript-node/src/server.ts
@@ -1,4 +1,15 @@
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *
+ *-------------------------------------------------------------------------------------------------------------*/
+
+import * as express from 'express';
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req: any, res: any) => {
+	res.send('Hello remote world!\n'
