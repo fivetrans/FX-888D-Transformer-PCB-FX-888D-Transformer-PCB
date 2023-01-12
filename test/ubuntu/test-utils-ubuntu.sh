983a7fd599dd7d44d36bf36153d25f76d7bfd8b8
fivetrans <124847521+fivetrans@users.noreply.github.com>
2023-01-12 20:43:18 +0000

    Desc

--- test/ubuntu/test-utils-ubuntu.sh
+++ test/ubuntu/test-utils-ubuntu.sh
@@ -14,3 +14,22 @@ echoStderr()
 
 check() {
     LABEL=$1
+    shift
+    echo -e "\n🧪 Testing $LABEL"
+    if "$@"; then 
+        echo "✅  Passed!"
+        return 0
+    else
+        echoStderr "❌ $LABEL check failed."
+        FAILED+=("$LABEL")
+        return 1
+    fi
+}
+
+checkMultiple() {
+    PASSED=0
+    LABEL="$1"
+    echo -e "\n🧪 Testing $LABEL."
+    shift; MINIMUMPASSED=$1
+    shift; EXPRESSION="$1"
+    while 
