48eacf40a66748c0da8321c7d6e771dcdf63f441
fivetrans <124847521+fivetrans@users.noreply.github.com>
2023-02-17 14:52:19 +0000

    init

--- test/ubuntu/test-utils-ubuntu.sh
+++ test/ubuntu/test-utils-ubuntu.sh
@@ -89,4 +89,10 @@ checkCommon()
 
     # Actual tests
     checkOSPackages "common-os-packages" ${PACKAGE_LIST}
-    checkMultiple "vscode-server" 1 "[ -d $HOME/.vscode-server/bin ]" "[ -d $HOME/.vscode-server-insiders/bin ]" "[ -d $HOME/.vscode-test-server/bin ]" "[ -d
+    checkMultiple "vscode-server" 1 "[ -d $HOME/.vscode-server/bin ]" "[ -d $HOME/.vscode-server-insiders/bin ]" "[ -d $HOME/.vscode-test-server/bin ]" "[ -d $HOME/.vscode-remote/bin ]" "[ -d $HOME/.vscode-remote/bin ]"
+    check "non-root-user" id ${USERNAME}
+    check "locale" [ $(locale -a | grep en_US.utf8) ]
+    check "sudo" sudo echo "sudo works."
+    check "zsh" zsh --version
+    check "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
+    check "login-shell-path" [ -f "/etc/profile.d/00-restore-env.
